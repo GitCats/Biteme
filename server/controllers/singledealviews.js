131badb488175cb21440abcdaf05ce38ec352ff1
Kayla Matteo <kaylamatteo@gmail.com>
2016-01-16 10:18:17 -0600

    more specific routes

--- server/controllers/singledealviews.js
+++ server/controllers/singledealviews.js
@@ -4,6 +4,8 @@ var singledealview = require('../models/singledealview.js');
 var router = express.Router();
 module.exports = router;
 
+//POST
+//this should take whichever deal is selected, and use the deal id to send back more information about that specific deal
 router.get('/', function (req, res) {
 	singledealview.view(req.body)
 	.then(function (result) {
--- server/controllers/userauths.js
+++ server/controllers/userauths.js
@@ -4,12 +4,23 @@ var Auth = require('../models/userauth.js');
 var router = express.Router();
 module.exports = router;
 
+//SIGN IN (POST)
+//when the user submits their username and password, this will check username against the database for a match.
+//if it finds a match it will check the password, and if that matches it sends a 200 (okay) response
 router.post('/', function (req, res) {
 	console.log('body', req.body)
 	Auth.signin(req.body).then(function (data) {
 		console.log('data password', data.password)
 		if(req.body.password === data[0].password){
-		res.sendStatus(201)
+		res.sendStatus(200)
 		}
 	})
+})
+
+//SIGN UP (POST)
+//when a user inputs a new username, this will check against the database to see if it already exists
+//if it doesn't then it will create it and save the password associated with it to the users database
+//then send back 201 (created) response
+router.post('/', function (req, res){
+	
 })
