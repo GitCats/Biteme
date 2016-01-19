abb18722e157123e14a174b7d65666427dd51e26
Kayla Matteo <kaylamatteo@gmail.com>
2016-01-19 14:22:25 -0600

    owner can now create a profile in db

--- server/models/owner.js
+++ server/models/owner.js
@@ -4,7 +4,7 @@ var Owner = module.exports
 
 //selects and returns all deals for the specified restaurant
 //only if they are not yet expired
-Owner.prevDeals = function(restaurantId) {
+Owner.allDeals = function(restaurantId) {
   return db('deals')
     .join('restaurants', 'deals.restaurant_id', '=', 'restaurants.restaurant_id')
     .select('restaurants.name', 'restaurants.image_name', 'deals.description', 'deals.expiration', 'deals.deal_id')
@@ -27,3 +27,15 @@ Owner.create = function(body) {
 	return db('deals')
 	.insert({restaurant_id: body.restaurant_id, description: body.description, expiration: body.expiration, month: body.month, day: body.day, year: body.year})
 }
+
+//**NEED TO CHECK FOR SIGN IN**
+Owner.setup = function(body){
+	return db('restaurants')
+	.insert({cuisine_id: body.cuisine_id, image_name: body.image_name, description: body.description, url: body.url, address: body.address})
+}
+
+//insert into restaurants (cuisine_id, image_name, username, password, name, description, url, address)
+//body will be {"cuisine_id": 7, 
+//"image_name": "http://sheehancan.com/bbq/wp-content/uploads/2015/05/421929_324378574277288_656785892_n.jpg", 
+//"description": "World Famous Barbecue", "url": "www.franklinbarbecue.com", "address": "900 East 11th Street, Austin, TX"}
+
