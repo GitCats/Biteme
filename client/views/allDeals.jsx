38f498477f3d25d14b9b1479e0aa27b3f171908d
Kayla Handy <kayla.j.handy@gmail.com>
2016-01-19 15:02:16 -0600

    Map is working

--- client/views/allDeals.jsx
+++ client/views/allDeals.jsx
@@ -2,13 +2,13 @@
 var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
-var IndexRoute = require('react-router').IndexRoute;
-var Link = require('react-router').Link
+// var Router = require('react-router').Router;
+// var Route = require('react-router').Route;
+// var IndexRoute = require('react-router').IndexRoute;
+// var Link = require('react-router').Link
 var Modal = require('react-modal');
-var Maps = require('google-maps')
-var Map = require('./Map.js')
+// var Maps = require('google-maps')
+var Map = require('./map.jsx')
 
 
 var Deal = React.createClass({
