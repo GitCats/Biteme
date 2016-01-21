13e9dddf05b8ae59be1c78b0987eed5e4cc64333
Salman Oskooi <soskooi@gmail.com>
2016-01-21 15:11:02 -0600

    got auto reroute from ownerProfile if owner isn't logged in; also unrequired some modules where they didn't seem to be doing anything

--- client/views/main.jsx
+++ client/views/main.jsx
@@ -1,22 +1,19 @@
-//might not need all of these - only $ react, reactdom, auth, link from SO
-// var $ = require('jquery');
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// var Router = require('react-router').Router;
-// var Route = require('react-router').Route;
-// var IndexRoute = require('react-router').IndexRoute;
-var Link = require('react-router').Link
 var IndexLink = require('react-router').IndexLink;
 var Signup = require('./auth.jsx').signup;
 var Login = require('./auth.jsx').login;
 var OwnerSignup = require('./auth.jsx').ownerSignup;
 var OwnerLogin = require('./auth.jsx').ownerLogin;
+var Link = require('react-router').Link
+
 
 var App = React.createClass({
   render: function() {
     return (
       <div className="logoDiv">
         <IndexLink to='/'><img src="client/assets/banner.jpg" className="bluePlateLogo" /></IndexLink>
+      {/*ADD CONDITIONAL STATEMENT HERE THAT CHECKS FOR LOGIN STATUS TO DISPLAY THE NEXT 2 LINES*/}
+      {/*IF localStorage.getItem("user"), IT SHOULD DISPLAY LINK TO USER PREFS PAGE*/}
+      {/*IF localStorage.getItem("restaurant_id"), IT SHOULD ALSO DISPLAY LINK TO OWNERPROFILE*/}
         <div className="auth">Care to filter by preferences? Click <Signup /> or <Login /> for this cool feature!</div>
         <span className="ownerAuth"><p className="text">Are you a <strong>restaurant owner</strong> with deals to offer? Click <OwnerSignup /> to sign up to list them, or <OwnerLogin /> if you already have an account.</p></span>
         {this.props.children}
--- client/views/ownerProfile.jsx
+++ client/views/ownerProfile.jsx
@@ -1,11 +1,10 @@
 var $ = require('jquery');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
+// var React = require('react');
+// var ReactDOM = require('react-dom');
 var Link = require('react-router').Link;
 var LinkedStateMixin = require('react-addons-linked-state-mixin');
-
+var OwnerSignup = require('./auth.jsx').ownerSignup;
+var OwnerLogin = require('./auth.jsx').ownerLogin;
 
 //OWNER PROFILE PAGE
 //
@@ -20,43 +19,47 @@ var LinkedStateMixin = require('react-addons-linked-state-mixin');
 //make a unique AJAX POST request to update only profile info, not deals (has to update all fields, must include "restaurant_id" identifier in req.body) => api/owner/updateprofile
 //
 //Displaying past deals <PastDeals />:
-//show restaurant name, cuisine, expiration date/time (hour/minute/ampm dropdowns), logo, & text of deal description (based on AJAX GET request referenced above)
+//show restaurant name, cuisine, expiration date/time (hour/minute/ampm dropdowns), logo, & text of deal description
+//based on AJAX GET request referenced above, must loop through all deals
 
 
 var OwnerProfile = React.createClass({
 
-  // componentDidMount: function() {
-  //   this.loadPriorSettings();
-  //   // setInterval(this.loadPriorSettings, 1000);
-  // },
+  render: function() {
+    if (localStorage.getItem("restaurant_id")) {
+      return (
+        <div>
+          <CreateDeal />
+          <OwnerForm />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>YOU ARE NOT LOGGED IN AS A RESTAURANT OWNER</h1>
+          <span className="ownerAuth"><p className="text">Are you a <strong>restaurant owner</strong> with deals to offer? Click <OwnerSignup /> to sign up to list them, or <OwnerLogin /> if you already have an account.</p></span>
+          <p className="text">If {"you're"} just looking for deals, please <Link to={"/"}>visit our main page here.</Link></p>
+        </div>
+      )
+    }
+  }
+});
 
-  // getInitialState: function() {
-  //   return {settings: {}};
-  // },
 
-  // loadPriorSettings: function() {
-  //   $.ajax({
-  //     url: 'api/owner/' + localStorage.getItem("restaurant_id"),
-  //     dataType: 'json',    /*defaults to GET request*/
-  //     success: function(settings) {
-  //       this.setState({settings: settings});
-  //       console.log("Owner settings:", this.state.settings);
-  //       console.log("Past set cuisine:", this.state.settings[0].cuisine_id);
-  //     }.bind(this),
-  //     error: function(xhr, status, err) {
-  //       console.error("XHR:", xhr, "\nstatus:", status, "\nError:", err.toString());
-  //     }.bind(this)
-  //   });
-  // },
+var CreateDeal = React.createClass({
 
   render: function() {
     return (
       <div>
-        <OwnerForm />
+        <h1>Create a Deal</h1>
+        <br/><br/>
+        When will your deal expire?: <input type="text" />
+        <br/><br/>
       </div>
     );
   }
 });
+
 
 var OwnerForm = React.createClass({
 
@@ -80,7 +83,8 @@ var OwnerForm = React.createClass({
         var settings = settings[0];
         console.log("Owner settings:", settings);
         var address = settings.address.split(",");
-        if (this.isMounted()) {
+        // if (this.isMounted()) {
+          this.setState({settings: settings}); //Needed to pass down all deals to PastDeals
           this.setState({
             cuisine: settings.cuisine_id,
             address: address[0],
@@ -88,51 +92,62 @@ var OwnerForm = React.createClass({
             state: address[2].substring(1, address[2].length - 6),
             zip: address[2].substr(address[2].length - 5)
           });
-        }
-        // console.log("Owner settings:", this.state.settings);
-        // console.log("Past set cuisine:", this.state.settings.cuisine_id);
+        // }
       }.bind(this),
       error: function(xhr, status, err) {
         console.error("XHR:", xhr, "\nstatus:", status, "\nError:", err.toString());
       }.bind(this)
-    // console.log("Dropdown menu value:", this.state.cuisine_id);
     });
   },
 
-  // selectCuisine: function(e) {
-  //   this.setState({cuisine: e.target.value});
-  // },
-
   render: function() {
-    // console.log("Current dropdown value:", this.state.cuisine);
     console.log("Dropdown menu value:", this.state.cuisine);
     return (
-      <form className="ownerForm">
-        Street Address: <input type="text" valueLink={this.linkState("address")} /> 
-        City: <input type="text" valueLink={this.linkState("city")} /> 
-        State: <input type="text" valueLink={this.linkState("state")} /> 
-        ZIP: <input type="text" valueLink={this.linkState("zip")} /> 
+      <div>
+        <h1>Create or Update Your Restaurant Profile</h1>
         <br/><br/>
-        <select valueLink={this.linkState("cuisine")} >
-          <option value="">-Choose your cuisine-</option>
-          <option value="1">Mexican</option>
-          <option value="2">Fast Food</option>
-          <option value="3">Pizza</option>
-          <option value="4">Sandwiches</option>
-          <option value="5">Burgers</option>
-          <option value="6">American</option>
-          <option value="7">BBQ</option>
-          <option value="8">Diner</option>
-          <option value="9">Chinese</option>
-          <option value="10">Italian</option>
-          <option value="11">Japanese</option>
-          <option value="12">Vietnamese</option>
-          <option value="13">Thai</option>
-          <option value="14">Steakhouse</option>
-          <option value="15">Indian</option>
-          <option value="16">Other</option>
-        </select>
-      </form>
+        <form className="ownerForm">
+          Street Address: <input type="text" valueLink={this.linkState("address")} /> 
+          City: <input type="text" valueLink={this.linkState("city")} /> 
+          State: <input type="text" valueLink={this.linkState("state")} /> 
+          ZIP: <input type="text" valueLink={this.linkState("zip")} /> 
+          <br/><br/>
+          <select valueLink={this.linkState("cuisine")} >
+            <option value="">-Choose your cuisine-</option>
+            <option value="1">Mexican</option>
+            <option value="2">Fast Food</option>
+            <option value="3">Pizza</option>
+            <option value="4">Sandwiches</option>
+            <option value="5">Burgers</option>
+            <option value="6">American</option>
+            <option value="7">BBQ</option>
+            <option value="8">Diner</option>
+            <option value="9">Chinese</option>
+            <option value="10">Italian</option>
+            <option value="11">Japanese</option>
+            <option value="12">Vietnamese</option>
+            <option value="13">Thai</option>
+            <option value="14">Steakhouse</option>
+            <option value="15">Indian</option>
+            <option value="16">Other</option>
+          </select>
+        </form>
+        <PastDeals settings={this.state.settings} />
+      </div>
+    );
+  }
+});
+
+
+var PastDeals = React.createClass({
+
+  render: function() {
+    console.log("PastDeals settings property:", this.props.settings);
+    return (
+      <div>
+        <h1>Past Deals</h1>
+        <br/><br/><br/><br/><br/>
+      </div>
     );
   }
 });
