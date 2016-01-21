4128470ee61c0b280f30c2aac195b01ccd1ed857
eazy-g <gbrustkern@gmail.com>
2016-01-21 16:57:54 -0600

    Owner's deals are now sorted, with soonest to expire/have expired first

--- server/models/owner.js
+++ server/models/owner.js
@@ -13,9 +13,13 @@ Owner.allDeals = function(url) {
 	var id = url.substr(url.lastIndexOf("/")+1);
 	console.log('id: ', id)
 	return db('deals')
+  .orderBy('year', 'asc')
+  .orderBy('month', 'asc')
+  .orderBy('day', 'asc')
+  .orderBy('expiration', 'asc')
 	.join('restaurants', 'deals.restaurant_id', '=', 'restaurants.restaurant_id')
   .select('restaurants.name', 'restaurants.image_name', 'restaurants.cuisine_id', 'restaurants.address', 'restaurants.url', 'restaurants.res_description', 'restaurants.phone_number', 'deals.description', 'deals.expiration', 'deals.deal_id', 'deals.month', 'deals.day', 'deals.year')
-    .where ('deals.restaurant_id', id)
+   .where ('deals.restaurant_id', id)
 };
 
 //this will check to see if the username is in the restaurant table
