9aba461c2cc76b4744b5fd74aed0dc1ace766b34
Kayla Matteo <kaylamatteo@gmail.com>
2016-01-21 15:05:05 -0600

    commit

--- client/views/main.jsx
+++ client/views/main.jsx
@@ -16,8 +16,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div className="logoDiv">
-      <div className="auth">Care to filter by preferences? <Signup /> or <Login /></div>
-      <div className="ownerAuth"><p className="text">Restaurant Owner? <OwnerSignup /> or <OwnerLogin /></p></div>
+      <div className="auth">Care to filter by preferences? <Signup /> or <Login /> </div>
         <IndexLink to='/'><img src="client/assets/banner.jpg" className="bluePlateLogo" /></IndexLink>
         {this.props.children}
       </div>
