d33c8896f30643aae0bc4540e55c2513b70e413a
Salman Oskooi <soskooi@gmail.com>
2016-01-24 18:03:38 -0600

    added some styling, link to homepage, and submission to db functionality to ownerProfile

--- client/views/ownerProfile.jsx
+++ client/views/ownerProfile.jsx
@@ -7,22 +7,19 @@ var Datetime = require('react-datetime');
 //
 //Creating new deals <CreateDeal />:
 //expiration (INTEGER, ex: 1700, 24 hr time, no colons), calendar select:(month(INT value), day, year)
-//submit button
 //sends unique AJAX POST => "api/owner/create/"+localStorage.getItem("restaurant_id")
 //re-renders past deals view to include latest deals (should also put allDeals view on a setInterval to update with this info)
 //
 //Displaying & Updating the restaurant profile <OwnerForm />:
-//submit button
 //concat address values & "trim" all values on submit
 //update GET request API route to "api/owner/getAllDeals/"+id after KaylaM's update
 //make a unique AJAX POST request to update only profile info, must include "restaurant_id" in req.body) => api/owner/updateprofile
 //
 //Displaying past deals <PastDeals />:
-//show restaurant name, cuisine, expiration date/time (hour/minute/ampm dropdowns), logo, & text of deal description
-//based on AJAX GET request referenced above, must loop through all deals
 //
 //When done check to see what empty settings do to a newly sign-up owner's profile
 //Place the above 3 sections in tabs
+
 
 //Note about this module: the AJAX request in OwnerForm and its state-setting arguably should 
 //have been done in the parent OwnerProfile component. But we learned a lot about React
@@ -87,6 +84,31 @@ var CreateDeal = React.createClass({
 
   chooseDate: function(momentObj) {
     this.setState({ totalExpiration: momentObj._d });
+    var time = this.state.totalExpiration;
+    console.log("Selected time:", time.getHours());
+    //we don't want 0 in front of hours less than 10, midnight should be 2400, all should be numbers, and months are mapped from 1 - 12
+  },
+
+  postDeal: function(e) {
+    e.preventDefault();
+    var description = this.state.description.trim();
+    
+    $.ajax({
+      url: ,
+      dataType: 'json',
+      type: 'POST',
+      data: ,
+      success: function(data) {
+        
+        this.setState({description: '', totalExpiration: new Date()});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        this.setState({data: comments});
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+
+
   },
 
   render: function() {
@@ -96,17 +118,20 @@ var CreateDeal = React.createClass({
     };
     return (
       <div>
-        <h1>Create a Deal</h1>
-        <br/><br/>
-        <h3>Make a deal for {this.props.initialData.name}</h3>
-        <br/>
-        <p>Describe your deal in a few words: </p>
-          <input type="text" valueLink={this.linkState("description")} size="40" maxLength="35" />
-        <br/><br/>
-        <p style={{textAlign: "center"}}>When will your deal expire?</p>
-        <div style={{textAlign: "center"}}>Click a calendar date to see type-in formatting.</div>
-        <Datetime open={ true } isValidDate={ valid } value={this.state.totalExpiration} onChange={this.chooseDate} />
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+        <form>
+          <h1>Create a Deal</h1>
+          <br/>
+          <h3 className="text">Make a deal for {this.props.initialData.name}</h3>
+          <br/>
+          <p className="text">Describe your deal in a few words: </p>
+            <input type="text" valueLink={this.linkState("description")} className="dealSubmit" size="40" maxLength="35" />
+          <br/><br/>
+          <p className="text">When will your deal expire?</p>
+          <Datetime open={ true } isValidDate={ valid } value={this.state.totalExpiration} onChange={this.chooseDate} />
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <input type="submit" className="dealSubmit" value="Post My Deal!" />
+          <br/><br/>
+        </form>
       </div>
     );
   }
@@ -166,43 +191,48 @@ var OwnerForm = React.createClass({
       <div>
         <h1>Create or Update Your Restaurant Profile</h1>
         <br/>
-        Restaurant name: <input type="text" valueLink={this.linkState("name")} />
-        <img src={this.state.logo} alt="Your Logo" className="dealLogo" style={{margin: 25}} />
-        Enter a new URL to update your logo: <input type="text" valueLink={this.linkState("logo")} size="70" />
-        <form className="ownerForm">
-          Street Address: <input type="text" valueLink={this.linkState("address")} /> 
-          City: <input type="text" valueLink={this.linkState("city")} /> 
-          State: <input type="text" valueLink={this.linkState("state")} /> 
-          ZIP: <input type="text" valueLink={this.linkState("zip")} maxLength="5" /> 
-          <br/><br/>
-          Phone number (which customers should use to call the restaurant): 
-          <input type="text" valueLink={this.linkState("phone")} maxLength="14" />
-          Business website: <input type="text" valueLink={this.linkState("website")} />
-          <br/><br/>Select the cuisine that best matches your restaurant:
-          <select valueLink={this.linkState("cuisine")} >
-            <option value="">-Choose your cuisine-</option>
-            <option value="1">Mexican</option>
-            <option value="2">Fast Food</option>
-            <option value="3">Pizza</option>
-            <option value="4">Sandwiches</option>
-            <option value="5">Burgers</option>
-            <option value="6">American</option>
-            <option value="7">BBQ</option>
-            <option value="8">Diner</option>
-            <option value="9">Chinese</option>
-            <option value="10">Italian</option>
-            <option value="11">Japanese</option>
-            <option value="12">Vietnamese</option>
-            <option value="13">Thai</option>
-            <option value="14">Steakhouse</option>
-            <option value="15">Indian</option>
-            <option value="16">Other</option>
-          </select>
-          <br/><br/>
-          <p>Describe your restaurant in a few lines:</p>
-          <textarea valueLink={this.linkState("res_description")} rows="3" cols="68" maxLength="200" />
+        <form>
+          Restaurant name: <input type="text" valueLink={this.linkState("name")} />
+          <img src={this.state.logo} alt="Your Logo" className="dealLogo" style={{margin: 25}} />
+          Enter a new URL to update your logo: <input type="text" valueLink={this.linkState("logo")} size="70" />
+          <div className="dealSubmit text">
+            Street Address: <input type="text" valueLink={this.linkState("address")} /> 
+            City: <input type="text" valueLink={this.linkState("city")} /> 
+            State: <input type="text" valueLink={this.linkState("state")} /> 
+            ZIP: <input type="text" valueLink={this.linkState("zip")} maxLength="5" /> 
+            <br/><br/>
+            Phone number (which customers should use to call the restaurant): 
+            <input type="text" valueLink={this.linkState("phone")} maxLength="14" />
+            Business website: <input type="text" valueLink={this.linkState("website")} />
+            <br/><br/>Select the cuisine that best matches your restaurant:
+            <select valueLink={this.linkState("cuisine")} >
+              <option value="">-Choose your cuisine-</option>
+              <option value="1">Mexican</option>
+              <option value="2">Fast Food</option>
+              <option value="3">Pizza</option>
+              <option value="4">Sandwiches</option>
+              <option value="5">Burgers</option>
+              <option value="6">American</option>
+              <option value="7">BBQ</option>
+              <option value="8">Diner</option>
+              <option value="9">Chinese</option>
+              <option value="10">Italian</option>
+              <option value="11">Japanese</option>
+              <option value="12">Vietnamese</option>
+              <option value="13">Thai</option>
+              <option value="14">Steakhouse</option>
+              <option value="15">Indian</option>
+              <option value="16">Other</option>
+            </select>
+            <br/><br/>
+            <p>Describe your restaurant in a few lines:</p>
+            <textarea valueLink={this.linkState("res_description")} rows="3" cols="68" maxLength="200" />
+            <br/><br/>
+            <input type="submit" value="Update Restaurant Profile" />
+          </div>
         </form>
         <PastDeals settings={this.state.settings} />
+        <Link to={"/"} className="dealSubmit text">Go to Main Page</Link>
       </div>
     );
   }
