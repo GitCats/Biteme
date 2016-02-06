265ecf34ea917f4ce0107929f645b23ea16634d3
eazy-g <gbrustkern@gmail.com>
2016-02-06 17:24:36 -0600

    removed cuisine selection section

--- client/views/userProfile.jsx
+++ client/views/userProfile.jsx
@@ -270,7 +270,6 @@ var UserProfile = React.createClass({
         var emailChecked = data[0]["email_notify"];
         emailChecked==='yes' ? emailChecked=true : emailChecked=false;
         var phoneHolio = data[0]["phone"];
-        console.log('phoneHolio', phoneHolio);
 
         this.setState({phoneOnOff: phoneChecked, emailOnOff: emailChecked, phoneNum: phoneHolio});
       }.bind(this),
@@ -384,7 +383,6 @@ var UserProfile = React.createClass({
       data: {"a": sending},
       success: function(data) {
         alert("Your changes have been saved!");
-        console.log('return data from phone', data);
       }.bind(this),
       error: function(xhr, status, err) {
         console.error('api/userprefs/phone', status, err.toString());
@@ -434,7 +432,7 @@ var UserProfile = React.createClass({
           <h2 className='userText'>Hello {user}</h2>
           <PhoneForm submitphone={this.handlePhoneChange} onRadioChange={this.handleRadioChange} phoneOnOff={this.state.phoneOnOff} emailOnOff={this.state.emailOnOff} phone={this.state.phoneNum} phoneChange={this.handlePhoneInput}/>
           <RestaurantForm data={this.state.restaurantPreferences} altered={this.state.resViewAltered} onBoxChange={this.handleResChange} submitChanges={this.submitResChange} />
-          <CuisineForm data={this.state.cuisinePreferences} altered={this.state.cuisineViewAltered} onBoxChange={this.handleBoxChange} submitChanges={this.submitCuisinesChange} />
+          {/*<CuisineForm data={this.state.cuisinePreferences} altered={this.state.cuisineViewAltered} onBoxChange={this.handleBoxChange} submitChanges={this.submitCuisinesChange} />*/}
         </div>
       );
     } else {
