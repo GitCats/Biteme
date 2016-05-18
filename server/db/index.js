3aa6aed90c06df39e0d0f37c1ede53af015b606c
eazy-g <gbrustkern@gmail.com>
2016-05-18 09:14:10 -0500

    db changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,17 @@
 var knex = require('knex')({
   client: 'postgresql',
   connection: {
-    host     : 'ec2-54-197-224-155.compute-1.amazonaws.com',
-    user     : 'afoezekohcntbe',
-    password : '7WY6zAYHlWlYWozkCrSnpqJa06',
+    // database: 'test_for_blueplate'
+    host: 'ec2-54-197-224-155.compute-1.amazonaws.com',
+    user: 'eagqukcdyrkmat',
+    password: 'zTD1WLZInN_gtjV4775AqCp20o',
     port: 5432,
-    database : 'df5rer6m8ffk07',
+    database: 'df5rer6m8ffk07'
     ssl: true
+  },
+  pool: {
+    min: 2,
+    max: 10
   }
 });
 console.log("Connected to Postgres");
