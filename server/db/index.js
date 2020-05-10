7452621f6cebd077ecb76d773913b85fb131e71a
Salman Oskooi <soskooi@gmail.com>
2020-05-10 03:24:16 -0500

    Update pg connect params

--- server/db/index.js
+++ server/db/index.js
@@ -2,11 +2,11 @@ var knex = require('knex')({
   client: 'postgresql',
   connection: {
     // database: 'test_for_blueplate'
-    host: 'ec2-54-197-224-155.compute-1.amazonaws.com',
-    user: 'eagqukcdyrkmat',
-    password: 'zTD1WLZInN_gtjV4775AqCp20o',
+    host: 'ec2-174-129-255-7.compute-1.amazonaws.com',
+    user: 'lhemxbjlbqrppx',
+    password: '0cdfd6702e1335dfcc8cfd83579406aba7c13297028d1d2b779ced39bd8f985e',
     port: 5432,
-    database: 'df5rer6m8ffk07',
+    database: 'd5f8gbv9n20htr',
     ssl: true
   },
   pool: {
